0c5d73a2fe1099c1d3c29d428a85e1a7cadbc302
FedeLebe <ruso44@gmail.com>
2021-11-11 21:52:11 -0300

    cambios puerto

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('public'));
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
-app.listen(3030, function () {
-  console.log('escuchando en el puerto 3030');
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3030, function () {
+  console.log('Escuchando en el puerto ' + process.env.PORT);
 });
