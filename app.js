0d016bae502a12c944b14c26274426d1d097ed72
FedeLebe <ruso44@gmail.com>
2021-11-16 20:56:46 -0300

    cambios generales

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3030;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
+app.get('/register', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 3030, function () {
-  console.log('Escuchando en el puerto ' + process.env.PORT);
+app.listen(port, () => {
+  console.log(`Escuchando en el puerto ${port}`);
 });
